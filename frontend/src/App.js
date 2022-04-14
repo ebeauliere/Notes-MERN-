d75a32b8010a8469de1cc0bb09c396a627e70c44
ebeauliere <ebeauliere@msn.com>
2022-04-13 21:12:50 -0400

    connect MongoDB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,22 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter, Route } from "react-router-dom";
+import MyNotes from "./screens/MyNotes/MyNotes";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <>
+      <BrowserRouter>
         <Header />
         <main>
-          <LandingPage />
+          <Route path="/" component={LandingPage} exact />
+          <Route path="/mynotes" component={() => <MyNotes />} />
         </main>
         <Footer />
-      </>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,12 +7,15 @@ import {
   Navbar,
   NavDropdown,
 } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="primary" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Notes - MERN</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">Notes - MERN</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="m-auto">
@@ -25,7 +28,9 @@ const Header = () => {
             </Form>
           </Nav>
           <Nav>
-            <Nav.Link href="#home">My Notes</Nav.Link>
+            <Nav.Link>
+              <Link to="/mynotes">My Notes</Link>
+            </Nav.Link>
             <NavDropdown title="Eddy Beauliere" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
 
